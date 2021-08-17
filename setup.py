fccf6fb35ef4cfccc4c94c9c880082e1a8354ad5
Greg Frank <gregfrank@gmail.com>
2021-08-17 16:45:26 -0400

    bump python version & repo url

--- setup.py
+++ setup.py
@@ -24,13 +24,13 @@ test_requirements = [
 setup(
     author="Shankar Rao Pandala",
     author_email="shankar.pandala@live.com",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Lazy Predict help build a lot of basic models without much code and helps understand which models works better without any parameter tuning",
     entry_points={"console_scripts": ["lazypredict=lazypredict.cli:main",],},
@@ -44,7 +44,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/shankarpandala/lazypredict",
-    version='0.2.9',
+    url="https://github.com/gregfrank/lazypredict",
+    version='0.3.0',
     zip_safe=False,
 )
